c6456c1807c36548798a15ef3eef1e0650e3c5ea
Vince Lim <vince_victor_lim@dlsu.edu.ph>
2024-02-12 22:25:44 +0800

    Update RFIDocumentGenerator.js

--- src/components/DocumentTemplate/RFIDocumentGenerator.js
+++ src/components/DocumentTemplate/RFIDocumentGenerator.js
@@ -6,31 +6,59 @@ const fs = require('fs');
 //   static generateRFIDocument() {
 //     return new Promise((resolve, reject) => {
 //       try {
-        const doc = new PDFDocument();
-        const stream = fs.createWriteStream('RFI_Document.pdf');
+  const doc = new PDFDocument();
+  const stream = fs.createWriteStream('RFI_Document_Template.pdf');
 
-        doc.pipe(stream);
+  doc.pipe(stream);
 
-        // Add content to the RFI document
-        // Add Header
-        doc.fontSize(24).text('Request for Information (RFI)', { 
-          align: 'center' 
-        });
-        doc.moveDown();
+  doc.fontSize(20)
+      .text('Request for Information (RFI)', { align: 'center' })
+      .moveDown();
+     
+  doc.fontSize(12)
+      .text('Project Details:', { underline: true })
+      .moveDown();
+     
+  // Insert project details
+  const projectDetails = {
+    Title: 'Project ABC',
+    Manager: 'John Doe',
+    Deadline: 'March 31, 2024',
+    To: 'Mr.Bean',
+    Attention:'Architect Lebron',
+    CC: 'Engr Kobe'
+    // Add more project details here as needed
+    };
+     
+    for (const [key, value] of Object.entries(projectDetails)) {
+        doc.text(`${key}: ${value}`);
+     }
+     
+    // Add space
+    doc.moveDown();
+     
+    // Insert RFI details
+    doc.text('RFI Details:', { underline: true }).moveDown();
+     
+    const rfiDetails = {
+        RFINo: 'RFI-001',
+        dateRequested: 'February 12, 2024',
+        requestedBy: 'Jane Smith',
+        // Add more RFI details here as needed
+    };
 
-        // Add Body
-        doc.fontSize(16).text('RFI Body', { 
-          align: 'center' 
-        });
-        doc.moveDown();
+    for (const [key, value] of Object.entries(rfiDetails)) {
+      doc.text(`${key}: ${value}`);
+   }
 
-        // Add Footer
-        doc.fontSize(12).text('RFI Footer', { 
-          align: 'center' 
-        });
-
-        // End the document
-        doc.end();
+    doc.moveDown();
+     
+    // Insert RFI Description
+    doc.text('RFI Description:', { underline: true }).moveDown();
+    doc.text(`Please provide information regarding...`);
+     
+     // Finalize PDF
+    doc.end();
 
 //         // Handle stream finish event to resolve the promise
 //         stream.on('finish', () => {
